94a5bd01ae738f591f214262031efecaae126fdf
davidroses02 <davidrosasalcaraz@gmail.com>
2021-10-05 19:46:52 +0200

    Js

---
+++ PrimerasActividades/ejer1/main.js
@@ -0,0 +1,9 @@
+
+    let meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio"
+    , "Septiembre", "Noviembre", "Diciembre"];
+
+    for (let i = 0; i < meses.length; i++) {
+        alert(meses[i]);
+    }
+
+
